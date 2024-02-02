ee6bf559e38e51497723b8e6462dfb47b8d022cd
Muhammed Ansar EN <ansarworkingspace@gmail.com>
2024-02-02 10:40:34 +0530

    vercel added

--- frontend/src/components/videoOnBody/VideoOnBody.jsx
+++ frontend/src/components/videoOnBody/VideoOnBody.jsx
@@ -1,78 +1,10 @@
 
 
-
-// import React, { useRef, useEffect, useState } from 'react';
-// import './VideoOnBody.css';
-
-// const VideoOnBody = () => {
-//   const videoRef = useRef(null);
-//   const [isPlaying, setIsPlaying] = useState(false);
-
-//   useEffect(() => {
-//     const textOnVideo = document.querySelector('.textOnvideo h3');
-//     textOnVideo.classList.add('show');
-//   }, []);
-
-//   useEffect(() => {
-//     const afterVideoText = document.querySelector('.afterVideoText h3');
-//     afterVideoText.classList.add('show');
-//   }, []);
-
-//   const togglePlay = () => {
-//     const video = videoRef.current;
-
-//     if (isPlaying) {
-//       video.pause();
-//     } else {
-//       video.play();
-//     }
-
-//     setIsPlaying(!isPlaying);
-//   };
-
-//   useEffect(() => {
-//     const video = videoRef.current;
-//     video.play(); // Start playing the video automatically
-//     setIsPlaying(true);
-
-//     // Clean up function to pause the video when the component is unmounted
-//     return () => {
-//       video.pause();
-//     };
-//   }, []); // Empty dependency array ensures the effect runs only once, similar to componentDidMount
-
-//   return (
-//     <div className='VideoOnBodyDiv'>
-//       <div className="textOnvideo">
-//         <h3>Watch this video</h3>
-//         <img src="leftArrow.png" alt="" />
-//       </div>
-//       <div className="videoFrame">
-//         <video
-//           ref={videoRef}
-//           onClick={togglePlay}
-//           controls={false}
-//           width="87%"
-//           height="95%"
-//           muted
-//           loop
-//         >
-//           <source src="mainIntro.mp4" type="video/mp4" />
-//           Your browser does not support the video tag.
-//         </video>
-//       </div>
-//       <div className="afterVideoText">
-//         <img src="RightArrow.png" alt="" />
-//         <h3>Follow the Process</h3>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default VideoOnBody;
-
 import React, { useRef, useEffect } from 'react';
 import './VideoOnBody.css';
+
+
+
 
 const VideoOnBody = () => {
   const videoRef = useRef(null);
